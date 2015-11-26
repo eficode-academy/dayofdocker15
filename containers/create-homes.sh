edc7dce50e9dd426f38051ad9a3a483994726829
Muhammad Kamran Azeem <kaz@praqma.com>
2015-11-26 15:28:29 +0100

    Introduced reverse proxy for jenkins, artifactory and registry - in httpd.conf . Docker-compose.yml is updated with links and un-necessay ports removed. create-home.sh removes the /opt/containers and then rebuilds it.

--- containers/create-homes.sh
+++ containers/create-homes.sh
@@ -2,7 +2,7 @@
 # Run this script as sudo
 echo "You should not be running any containers in /opt/containers when you run this script."
 echo "Removing old directory structure from /opt/containers"
-rm /opt/containers/*
+rm /opt/containers/* -fr
 echo "Re-creating directory structure inside /opt/containers"
 mkdir -p /opt/containers/jenkins_home 
 mkdir -p /opt/containers/artifactory/data
